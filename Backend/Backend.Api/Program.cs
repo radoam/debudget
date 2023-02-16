505a313b6d3e5ca7663406f71b1b952e4aae0542
radoam <rluczak@egnyte.com>
2023-02-16 10:58:14 +0100

    Global error handling

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -1,5 +1,7 @@
+using Backend.Api.Errors;
 using Backend.Application;
 using Backend.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,10 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, DebudgetProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
